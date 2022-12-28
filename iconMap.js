5478778320283912dabfcb60c4e5cdbaef00df35
Derek Hoang <derekhoang@rocketmail.com>
2022-12-28 01:27:34 -0800

    First Push

---
+++ iconMap.js
@@ -0,0 +1,15 @@
+export const ICON_MAP = new Map();
+
+addMapping([0, 1], "day");
+addMapping([2], "cloudy-day");
+addMapping([3], "cloudy");
+addMapping([45, 48], "fog");
+addMapping([51, 53, 55, 56, 57, 61, 63, 65, 66, 67, 80, 81, 82], "rainy");
+addMapping([71, 73, 75, 77, 85, 86], "snowy");
+addMapping([95, 96, 99], "thunder");
+
+function addMapping(values, icon) {
+  values.forEach((value) => {
+    ICON_MAP.set(value, icon);
+  });
+}
